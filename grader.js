188feb68e7a5dd392a3289c730c7a87f990e67e4
zsolt deri <zsolt.deri@gmail.com>
2013-07-09 21:01:21 +0000

    grader.js from url

--- grader.js
+++ grader.js
@@ -26,31 +26,44 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLDEFAULT = "http://floating-woodland-5410.herokuapp.com/";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var urlExists = function(url) {
+    var instr = url.toString();
+    rest.get(url).on('error', function(err, response) {
+	if (err instanceof Error) {
+	console.log("Error, url doesn't exists: " + response);
+	process.exit(1);
+	}
+    });
+    return instr;
+};
+
+var cheerioHtmlFile = function(htmlContent) {
+    return cheerio.load(htmlContent);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlContent, checksfile) {
+    $ = cheerioHtmlFile(htmlContent);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -63,12 +76,31 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+	.option('-u, --url <url_reference>', 'Url reference', clone(urlExists))
+	.parse(process.argv);
+    if (program.file) {
+	var checkJson = checkHtmlFile(fs.readFileSync(program.file), program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+    else if (program.url) {
+	rest.get(program.url).once('complete', function(data) {
+	if (data instanceof Error) {
+	sys.puts('Error: ' + result.message);
+	} else {
+//	console.log(result);
+	var checkJson = checkHtmlFile(data, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+	}
+	});
+    }
+    else {
+	console.log('Error: invalid args, for help grader.js --help');
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
