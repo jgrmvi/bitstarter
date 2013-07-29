3dd545ac1ab2c88fa1e3871d0c44f126a0518abc
Zsolt Deri <zsolt.deri@gmail.com>
2013-07-29 12:50:09 +0000

    new style

--- web.js
+++ web.js
@@ -5,12 +5,15 @@ var INDEX='index.html';
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
   var fileContent = fs.readFileSync(INDEX).toString();
   response.send(fileContent);
 });
 
 var port = process.env.PORT || 8080;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
